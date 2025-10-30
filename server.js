daa64ae84e454edef14df9c33594d186572ee7f0
deograciasong <77689676+deograciasong@users.noreply.github.com>
2025-10-30 12:44:22 -0700

    fixed import routes

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./src/config/db.js";
-import authRoutes from "./src/routes/authRoutes.js";
-import playlistRoutes from "./src/routes/playlistRoutes.js";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -13,7 +12,6 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/auth", authRoutes);
-app.use("/api/playlist", playlistRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
