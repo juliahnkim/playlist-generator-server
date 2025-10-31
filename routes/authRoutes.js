f66ee6847dc5487affb2829da864b7505abe9f6e
Julia Kim <juliahnkim@gmail.com>
2025-10-31 09:23:17 -0700

    feat: restructure project by moving files to src directory

--- routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loginSpotify, callbackSpotify } from "../controllers/authController.js";
-const router = express.Router();
-
-router.get("/spotify", loginSpotify);
-router.get("/callback", callbackSpotify);
-
-export default router;
--- seeds/moodsSeeder.js
+++ seeds/moodsSeeder.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { Mood } from "../models/index.js";
+import { Mood } from "../src/models/index.js";
 
 dotenv.config();
 
